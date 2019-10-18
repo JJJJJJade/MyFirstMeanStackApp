1a000fa7e363786bc95064f97cd40420001dee04
ysq MacBook <jadeyan97@gmail.com>
2019-10-18 13:44:31 -0700

    lecture 18 finished

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  enteredvalue = '';
+  newPost = 'NO CONTENT';   // a property to save the new posts, default is an empty string
 
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   // tell typescript what type of element it is by A : B
+  //   // console.log(postInput);  //cannot see the content
+  //   // console.dir(postInput);
+  //   this.newPost = postInput.value;
+  // }
+
+  onAddPost() {
+    this.newPost = this.enteredvalue;
+  }
 }
