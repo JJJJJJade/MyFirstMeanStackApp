e3abb245c86003b8e41ccfa1eeea705895474adb
ysq MacBook <jadeyan97@gmail.com>
2019-10-18 15:09:18 -0700

    Lecture 21 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {MatInputModule, MatCardModule, MatCard, MatButtonModule,MatToolbarModule} from '@angular/material';
+import {MatInputModule, MatCardModule, MatCard, MatButtonModule,MatToolbarModule, MatExpansionModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent} from './header/header.component'
+import { HeaderComponent} from './header/header.component';
+import { PostListComponent} from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { HeaderComponent} from './header/header.component'
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
